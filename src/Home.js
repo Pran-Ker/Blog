ab8d1e667ce6e69f52cf6579d8cfd91dcca2db2d
Pran-Ker <hebbarpran@gmail.com>
2022-10-03 22:49:27 +0530

    Refactoring

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+      })
+    }, 1000);
+  }, [])
 
   return (
     <div className="home">
-      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} /> }
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
